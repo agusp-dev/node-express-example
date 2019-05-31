d1060f8c9f05d9d874966968a67a497d2dfab70a
Perez Agustin <agustn.jorgex.prez.pereyra@intel.com>
2019-05-31 16:12:24 -0300

    Implementacion de params
    Se implementa metodo get con query params y con path params.

--- server.js
+++ server.js
@@ -9,13 +9,28 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+//With query params
 app.get('/sayHello', function(req, res) {
-    console.log('Pedido recibido');
-    res.send('Hello from Server!!!');
+    console.log('New request');
+    var name = req.query.name;
+    res.send('Hello from Server!!! Nice to meet you ' + name);
 });
 
-var port = '8080';
+//With path params
+app.get('/sayHello/:name', function(req, res) {
+    console.log('New request');
+    var name = req.params.name;
+    res.send('Hello from Server!!! Nice to meet you ' + name);
+});
+
+var port = '8082';
 
 app.listen(port, function() {
     console.log('Server listening in ' + port + ' port...');
+});
+
+process.on('SIGTERM', function() {
+    server.close(function() {
+      console.log('Process terminated');
+    });
 });
